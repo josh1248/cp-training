4c47432234ffcfff3ff57b4df4a3209c5e6dcb78
josh1248 <joshthoo@gmail.com>
2025-01-04 22:41:18 +0800

    refile

--- codeforces/hello 2025/A.cpp
+++ codeforces/hello 2025/A.cpp
@@ -16,4 +16,11 @@ typedef long long ll;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    int r;
+    cin >> r;
+    for (int i = 0; i < r; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << max(a, b) + 1 << "\n";
+    }
 }
