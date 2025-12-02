46d96797ccbfc382299ecf691003971b9ae3da2e
josh1248 <joshthoo@gmail.com>
2025-12-02 11:15:21 +0100

    Advent of Code 2025, day 1

---
+++ adventofcode/2025/code/day1.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+
+int main() {
+    ifstream ins("../data/day1.txt");
+
+    if (!ins) {
+        return 1;
+    }
+
+
+    vi rots; string input_str;
+    while (ins >> input_str) {
+        int rot = stoi(input_str.substr(1));
+        rots.push_back(input_str[0] == 'R' ? rot : -rot);
+    }
+
+    ins.close();
+
+    int curr = 50;
+    int part1 = 0, part2 = 0;
+    for (int& rot: rots) {
+        int nxt = ((curr + rot) % 100 + 100) % 100;
+
+        if (nxt == 0) part1++;
+
+        if (curr + rot >= 100) {
+            part2 += (curr + rot) / 100;
+        } else if (curr + rot <= 0) {
+            part2 += abs(curr + rot - 100) / 100;
+            if (curr == 0) part2--;
+        }
+
+        curr = nxt;
+    }
+    
+
+
+    cout << "part 1: " << part1 << "\n";
+    cout << "part 2: " << part2 << "\n";
+}
